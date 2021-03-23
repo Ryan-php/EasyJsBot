9bc85cb7788abbb3c86bff97acff28a359356098
Gaëtan H <steellgold0@gmail.com>
2021-03-23 07:19:36 +0100

    Add console logger

--- utils/Logger.js
+++ utils/Logger.js
@@ -1,1 +1,34 @@
-# Todo
+const Colors = require('colors')
+# npm install colors
+
+const Main = require('../index.js')
+
+exports.error = (content) => {
+    content.split('\n').forEach(s => {
+        console.log(`${'[X]'.red} ${s}`)
+    })
+}
+
+exports.success = (content) => {
+    content.split('\n').forEach(s => {
+        console.log(`${'[✓]'.green} ${s}`)
+    })
+}
+
+exports.mod = (content) => {
+    content.split('\n').forEach(s => {
+        console.log(`${'[✓]'.green} ${s}`)
+    })
+}
+
+exports.loading = (content) => {
+    content.split('\n').forEach(s => {
+        console.log(`${'[?]'.yellow} ${s}`)
+    })
+}
+
+exports.loading = (content) => {
+    content.split('\n').forEach(s => {
+        console.log(`${'[...]'.black} ${s}`)
+    })
+}
