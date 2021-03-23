e5c322f3f8991eb5b0a7158eeb148789eab2c83d
Gaëtan H <steellgold0@gmail.com>
2021-03-23 07:24:18 +0100

    Prêt

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ const Client = new Discord.Client();
 const Logger = require('./utils/Logger.js');
 const Config = require('./utils/Config.json');
 
+Logger.SuccessLog("Prêt à écouter");
 Client.connect(Config.token);
