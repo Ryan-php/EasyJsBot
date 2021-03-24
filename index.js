785eb34678eb84fb8c58421edb134342ffda23d8
Gaëtan H <steellgold0@gmail.com>
2021-03-24 07:13:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 /* Déclaration des variables */
 const Discord = require('discord.js');
-const Client = new Discord.Client();
+exports.Client = new Discord.Client();
+
 const Logger = require('./utils/Logger.js');
 const Config = require('./utils/Config.json');
+const fs = require('fs');
 
+exports.Prefix = Config.prefix;
+
+/* Commandes */
+exports.Client.commands = new Discord.Collection();
+exports.argv = process.argv;
+
+loadCommand('./commands/');
+
+function loadCommand(path)  {
+	fs.readdir(path, (err, files) => {
+		if (err) console.log(err);
+
+		let jsfile = files.filter(f => f.split(".").pop() === "js")
+		if (jsfile.length <= 0) {
+			logger.error("Aucunes commandes trouvées dans " + path);
+			return;
+		}
+
+		jsfile.forEach((f, i) => {
+			let props = require(`${path}${f}`);
+			Logger.SuccessLog(`Commande ${f} opérationel`);
+			exports.Client.commands.set(props.help.name, props);
+		});
+	});
+}
+
+/* Événements */
+require('./events/OnMessage.js');
+
+/* Connexion */
 Logger.SuccessLog("Prêt à écouter");
-Client.connect(Config.token);
+exports.Client.login(Config.token);
